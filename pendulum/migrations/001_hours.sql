3ca3294c683dc1a5aca22cca617d2e9310e418ae
Colin Copeland <copelco@caktusgroup.com>
2009-10-21 23:21:51 -0400

    update migration to work with entries without clock time

--- pendulum/migrations/001_hours.sql
+++ pendulum/migrations/001_hours.sql
@@ -1,6 +1,6 @@
 BEGIN;
 ALTER TABLE pendulum_entry ADD COLUMN hours NUMERIC(8, 2);
-UPDATE pendulum_entry SET hours = ROUND(CAST((EXTRACT (EPOCH FROM (end_time - start_time)) - seconds_paused) / 3600 AS NUMERIC), 2);
+UPDATE pendulum_entry SET hours = COALESCE(ROUND(CAST((EXTRACT (EPOCH FROM (end_time - start_time)) - seconds_paused) / 3600 AS NUMERIC), 2), 0.0);
 COMMIT;
 
 BEGIN;
