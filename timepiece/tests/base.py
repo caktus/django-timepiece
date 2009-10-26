5e006c25a59b4bac2d3b69b6786b7ead252004f8
Colin Copeland <copelco@caktusgroup.com>
2009-10-25 22:07:23 -0400

    add billing period generation test

---
+++ timepiece/tests/base.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
+
+from timepiece import models as timepiece
+from crm import models as crm
+
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'u@abc.com', 'abc')
+        self.user2 = User.objects.create_user('user2', 'u2@abc.com', 'abc')
+        permissions = Permission.objects.filter(
+            content_type=ContentType.objects.get_for_model(timepiece.Entry),
+            codename__in=('can_clock_in', 'can_clock_out', 'can_pause')
+        )
+        self.user.user_permissions = permissions
+        self.user2.user_permissions = permissions
+        self.activity = timepiece.Activity.objects.create(
+            code="WRK",
+            name="Work",
+        )
+        self.business = crm.Contact.objects.create(
+            name='Example Business',
+            slug='example-business',
+            sort_name='example-business',
+            type='business',
+            description='',
+        )
+        self.project = timepiece.Project.objects.create(
+            name='Example Project',
+            business=self.business,
+            point_person=self.user,
+        )
+        self.project = timepiece.Project.objects.create(
+            name='Example Project',
+            business=self.business,
+            point_person=self.user2,
+        )
---
+++ timepiece/tests/timesheet.py
@@ -0,0 +1,82 @@
+import time
+import datetime
+import random
+import itertools
+
+from django.core.urlresolvers import reverse
+
+from timepiece.tests.base import BaseTest
+
+from timepiece import models as timepiece
+
+from timepiece.views import update_billing_windows
+
+from dateutil import relativedelta
+
+
+class ClockInTest(BaseTest):
+    def setUp(self):
+        super(ClockInTest, self).setUp()
+        self.url = reverse('timepiece-clock-in')
+    
+    def testClockInLogin(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username='user', password='abc')
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+    
+    def testClockIn(self):
+        self.client.login(username='user', password='abc')
+        response = self.client.post(self.url, {'project': self.project.id})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(timepiece.Entry.objects.count(), 1)
+
+
+class ClockOutTest(BaseTest):
+    def setUp(self):
+        super(ClockOutTest, self).setUp()
+    
+    def testBasicClockOut(self):
+        entry = timepiece.Entry.objects.create(
+            user=self.user,
+            project=self.project,
+            activity=self.activity,
+            start_time=datetime.datetime.now() - datetime.timedelta(hours=5),
+        )
+        self.client.login(username='user', password='abc')
+        self.client.post(reverse('timepiece-clock-out', args=[entry.pk]))
+        entry = timepiece.Entry.objects.get(pk=entry.pk)
+        self.assertTrue(entry.is_closed)
+
+
+def previous_and_next(some_iterable):
+    prevs, items, nexts = itertools.tee(some_iterable, 3)
+    prevs = itertools.chain([None], prevs)
+    nexts = itertools.chain(itertools.islice(nexts, 1, None), [None])
+    return itertools.izip(prevs, items, nexts)
+
+
+class BillingPeriodTest(BaseTest):
+    def testGeneratedWindows(self):
+        delta = relativedelta.relativedelta(days=random.randint(20, 100))
+        start_date = datetime.datetime.today() - delta
+        for count in range(1, 30):
+            for period, _ in timepiece.RepeatPeriod.INTERVAL_CHOICES:
+                p = self.project.billing_periods.create(
+                    count=count,
+                    interval=period,
+                    active=True,
+                )
+                window = p.billing_windows.create(
+                    date=start_date,
+                    end_date=start_date + p.delta(),
+                )
+                windows = update_billing_windows()
+                for prev, curr, next in previous_and_next(windows):
+                    if curr and prev:
+                        diff = \
+                            relativedelta.relativedelta(curr.date, prev.date)
+                        self.assertEqual(prev.date + diff, curr.date)
+                        self.assertEqual(prev.end_date, curr.date)
+                p.delete()
