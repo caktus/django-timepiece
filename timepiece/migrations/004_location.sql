b2e8f84c3a59d327b9bcbdf65ca8759563f800bc
Michael Clemmons <glassresistor@gmail.com>
2010-02-12 11:47:30 -0500

    set SRC as an option

--- timepiece/migrations/004_location.sql
+++ timepiece/migrations/004_location.sql
@@ -5,8 +5,10 @@ CREATE TABLE "timepiece_location" (
 )
 ;
 INSERT INTO "timepiece_location" ("name") VALUES ('Office');
-ALTER TABLE timepiece_entry DROP COLUMN "location";
+INSERT INTO "timepiece_location" ("name") VALUES ('SRC');
 ALTER TABLE timepiece_entry ADD COLUMN "location_id" integer;
 ALTER TABLE timepiece_entry ADD CONSTRAINT timepiece_entry_location_id_fkey FOREIGN KEY (location_id) REFERENCES timepiece_location (id) DEFERRABLE INITIALLY DEFERRED;
-UPDATE timepiece_entry SET location_id=1;
+UPDATE timepiece_entry SET location_id=2 WHERE location like 'SRC';
+UPDATE timepiece_entry SET location_id=1 WHERE location not like 'SRC';
+ALTER TABLE timepiece_entry DROP COLUMN "location";
 COMMIT;
