8965bf1cffa0310d94deed248841e45722f8bc84
Colin Copeland <copelco@caktusgroup.com>
2010-11-19 15:00:43 -0500

    remove old migrations
    --HG--
    branch : billable

--- timepiece/migrations/004_project_scheduling_hours.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-ALTER TABLE timepiece_personschedule ALTER COLUMN "hours_per_week" type numeric(8, 2);
-ALTER TABLE timepiece_projectcontract ALTER COLUMN "num_hours" type numeric(8, 2);
-ALTER TABLE timepiece_contractassignment ALTER COLUMN "num_hours" type numeric(8, 2);
-COMMIT;
