aa24b5f187bd19e35c2d0a8ae9ce98328012d475
Colin Copeland <copelco@caktusgroup.com>
2011-01-03 11:02:33 -0500

    stub for billable widget
    --HG--
    branch : project-budgeting

--- timepiece/widgets.py
+++ timepiece/widgets.py
@@ -1,9 +1,11 @@
+import re
+import json
 from decimal import Decimal
 
 from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
-import re
+
 
 re_id = re.compile('id="([^"]+)"')
 
@@ -71,3 +73,28 @@ class SecondsToHoursWidget(forms.TextInput):
             if is_integer:
                 value = round(float(value)/3600.0, 2)
         return super(SecondsToHoursWidget, self).render(name, value, attrs)
+
+
+class ToggleBillableWidget(forms.Select):
+    def __init__(self, billable, *args, **kwargs):
+        self.billable_map = billable
+        super(ToggleBillableWidget, self).__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None, choices=()):
+        output = super(ToggleBillableWidget, self).render(name, value, attrs,
+                                                          choices)
+        return output + """
+        <script type='text/javascript'>
+        var billable_map = %s;
+        console.log(billable_map);
+        jQuery(function() {
+            console.log(billable_map);
+            jQuery('#id_project').change(function() {
+                console.log(billable_map[jQuery(this).val()]);
+                jQuery('#id_billable').attr('checked', billable_map[jQuery(this).val()]);
+            });
+        });
+        </script>
+        """ % json.dumps(self.billable_map)
+        
+
