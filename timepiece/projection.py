6ce68f65e35767b463d90c457789532b21673527
Colin Copeland <copelco@caktusgroup.com>
2011-01-15 17:06:51 -0500

    update budgeting view to use contract block model; add weekly summary at bottom of page
    --HG--
    branch : projection

--- timepiece/projection.py
+++ timepiece/projection.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.conf import settings
+
 from dateutil import rrule
 
 from timepiece import models as timepiece
@@ -5,11 +9,12 @@ from timepiece import models as timepiece
 
 def run_projection():
     timepiece.ContractBlock.objects.all().delete()
-    contracts = timepiece.ProjectContract.objects.filter(status='current')
+    contracts = timepiece.ProjectContract.objects.exclude(status='complete')
     for contract in contracts:
         weekdays = rrule.rrule(rrule.DAILY, dtstart=contract.start_date,
-                               until=contract.end_date, byweekday=range(0, 5))
-        for assignment in contract.assignments.all():
+                               until=contract.end_date - datetime.timedelta(days=1), byweekday=range(0, 5))
+        assignments = contract.assignments.exclude(contract__project__in=settings.TIMEPIECE_PROJECTS.values())
+        for assignment in assignments:
             hours = assignment.hours_remaining/weekdays.count()
             for weekday in weekdays:
                 assignment.blocks.create(date=weekday, hours=hours) 
