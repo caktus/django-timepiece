a6c1ea2ddab268f471fc62eca572d817555ee997
Michael Clemmons <mike@caktusgroup.com>
2011-04-12 10:31:17 -0400

    switched to using lambda instead of function
    --HG--
    branch : project-budgeting

--- timepiece/templatetags/math_tags.py
+++ timepiece/templatetags/math_tags.py
@@ -2,9 +2,5 @@ from django import template
 
 register = template.Library()
 
-@register.filter
-def _abs(num):
-    return abs(num)
-
-register.filter('abs', _abs)
+register.filter('abs', lambda x: abs(x))
 
