917128bb9eee6ce70f98f85a1558bfadc71efd56
Michael Clemmons <mike@caktusgroup.com>
2011-04-12 11:07:04 -0400

    added error handling on the abs filter and cleaned up the pie chart positioning
    --HG--
    branch : project-budgeting

--- timepiece/templatetags/math_tags.py
+++ timepiece/templatetags/math_tags.py
@@ -2,5 +2,13 @@ from django import template
 
 register = template.Library()
 
-register.filter('abs', lambda x: abs(x))
+@register.filter
+def _abs(num):
+    try:
+        num = float(num)
+    except ValueError:
+        return 0
+    return abs(num)
+
+register.filter('abs', _abs)
 
