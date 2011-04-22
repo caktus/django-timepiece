c5afea9c423d21c244b0f1d8ab7a821b97fc06c9
Michael Clemmons <mike@caktusgroup.com>
2011-04-22 15:14:06 -0400

    added icons
    --HG--
    branch : feature/no-crm

--- timepiece/context_processors.py
+++ timepiece/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from timepiece.forms import QuickSearchForm
 
 def timepiece_settings(request):
-    default_famfamfam_url = settings.MEDIA_URL + 'images/icons/'
+    default_famfamfam_url = settings.STATIC_URL + 'images/icons/'
     famfamfam_url = getattr(settings, 'FAMFAMFAM_URL', default_famfamfam_url)
     context = {
         'FAMFAMFAM_URL': famfamfam_url,
