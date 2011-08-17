ac8edb38398ab8d484032923dfede34ec34334be
Caleb Smith <caleb.smithnc@gmail.com>
2011-08-17 13:59:45 -0400

    Backing out changes

--- timepiece/models.py
+++ timepiece/models.py
@@ -309,7 +309,8 @@ class Entry(models.Model):
         if end <= start:
             raise ValidationError('Ending time must exceed the starting time')
             
-        return True        
+        return True
+        
            
     def save(self, **kwargs):
         self.hours = Decimal('%.2f' % round(self.total_hours, 2))    
