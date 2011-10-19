f64d974f0a3b16a6c167d6c3e702566151315ed3
Caleb Smith <csmith@caktusgroup.com>
2011-10-19 16:35:10 -0400

    [#1] Fixes from master that didn't merge correctly. Should fix Jenkins

--- timepiece/management/commands/check_entries.py
+++ timepiece/management/commands/check_entries.py
@@ -37,6 +37,19 @@ For options type:
         Define the arguments that can be used with this command
         """
         return (
+        #Jenkins arguments to ignore
+        make_option('--pep8-exclude',
+            dest='ignore_pep8',
+            type='str',
+            default='',
+            help='Jenkins only'),
+        ) + (
+        make_option('--coverage-exclude',
+            dest='ignore_coverage',
+            type='str',
+            default='',
+            help='Jenkins only'),
+        ) + (
         make_option('--thisweek',
             action='store_true',
             dest='week',
