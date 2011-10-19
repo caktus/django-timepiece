88d96ebbe93f9b27d5dc7a08ceab0e52af73f441
Caleb Smith <csmith@caktusgroup.com>
2011-10-19 16:15:35 -0400

    Revert "[#1] Added check entry management command test"
    This reverts commit 07cdc01e7213d775e99ded0cbcd777feac4eca73.

--- timepiece/tests/__init__.py
+++ timepiece/tests/__init__.py
@@ -4,4 +4,5 @@ from timepiece.tests.payroll import *
 from timepiece.tests.projection import *
 from timepiece.tests.projects import *
 from timepiece.tests.user_settings import *
-from timepiece.tests.management import *
+#from timepiece.tests.management import *
+#Test does not reflect current state of the code. Import here when fixed
