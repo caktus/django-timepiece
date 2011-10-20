c071e3f2e80c5e71d0c1108729333696588bba93
Colin Copeland <copelco@caktusgroup.com>
2011-10-20 17:17:19 -0400

    update version; add MANIFEST; fill out setup.py meta data

--- setup.py
+++ setup.py
@@ -10,9 +10,19 @@ setup(
     exclude_package_data={
         '': ['*.sql', '*.pyc',],
     },
-    url='',
+    url='https://github.com/caktus/django-timepiece',
     license='BSD',
-    description='django-timepiece',
     long_description=open('README.rst').read(),
     zip_safe=False, # because we're including media that Django needs
+    description="django-timepiece is a multi-user application for tracking "
+                "people's time on projects.",
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
 )
