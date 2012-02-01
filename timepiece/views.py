6afdd9265644404a13e8b889278c67a70a8a9f3d
Colin Copeland <copelco@caktusgroup.com>
2012-02-01 08:35:06 -0500

    redirect to invoice view on save; closes #140

--- timepiece/views.py
+++ timepiece/views.py
@@ -685,7 +685,7 @@ def confirm_invoice_project(request, project_id, to_date, from_date=None):
         invoice = invoice_form.save()
         entries = timepiece.Entry.objects.filter(**entries_query)
         entries.update(status=invoice.status, entry_group=invoice)
-        return HttpResponseRedirect(reverse('invoice_projects'))
+        return HttpResponseRedirect(reverse('view_invoice', args=[invoice.pk]))
     else:
         entries = timepiece.Entry.objects.filter(**entries_query)
         entries = entries.order_by('start_time')
