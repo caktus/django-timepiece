9173f927610e469f80c3cb0d0bc91ab6897e490c
Caleb Smith <csmith@caktusgroup.com>
2012-02-20 10:26:02 -0500

    refs #78 - Added some basic tests for project timesheet

--- timepiece/tests/projects.py
+++ timepiece/tests/projects.py
@@ -1,3 +1,6 @@
+import datetime
+from decimal import Decimal
+
 from django.core.urlresolvers import reverse
 
 from timepiece import models as timepiece
@@ -5,6 +8,36 @@ from timepiece.tests.base import TimepieceDataTestCase
 
 
 class ProjectTestCase(TimepieceDataTestCase):
+
+    def setUp(self):
+        super(ProjectTestCase, self).setUp()
+        self.p1 = self.create_project(billable=True, name='1')
+        self.p2 = self.create_project(billable=False, name='2')
+        self.p4 = self.create_project(billable=True, name='4')
+        self.p3 = self.create_project(billable=False, name='1')
+        self.url = reverse('project_time_sheet',
+                           kwargs={'pk': self.p1.pk,}
+        )
+
+    def make_entries(self):
+        days = [
+                datetime.datetime(2011, 1, 1),
+                datetime.datetime(2011, 1, 28),
+                datetime.datetime(2011, 1, 31),
+                datetime.datetime(2011, 2, 1),
+                datetime.datetime.now(),
+        ]
+        self.log_time(project=self.p1, start=days[0], delta=(1, 0))
+        self.log_time(project=self.p2, start=days[0], delta=(1, 0))
+        self.log_time(project=self.p1, start=days[1], delta=(1, 0))
+        self.log_time(project=self.p3, start=days[1], delta=(1, 0))
+        self.log_time(project=self.p1, user=self.user2, start=days[2],
+                      delta=(1, 0))
+        self.log_time(project=self.p2, start=days[2], delta=(1, 0))
+        self.log_time(project=self.p1, start=days[3], delta=(1, 0))
+        self.log_time(project=self.p3, start=days[3], delta=(1, 0))
+        self.log_time(project=self.p1, start=days[4], delta=(1, 0))
+        self.log_time(project=self.p3, start=days[4], delta=(1, 0))
 
     def test_remove_user(self):
         self.user.is_superuser = True
@@ -28,3 +61,59 @@ class ProjectTestCase(TimepieceDataTestCase):
         response = self.client.post(url, {'user': self.user.pk, })
         self.assertEquals(response.status_code, 302)
         self.assertEquals(self.project.users.all().count(), 1)
+
+    def testEmptyProjectTimesheet(self):
+        """
+        The project timesheet should be empty if there are no entries, or a
+        month has been selected for which there are no entries
+        """        
+        self.client.login(username='superuser', password='abc')
+        def verify_empty(response):
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.context['project'], self.p1)
+            self.assertEqual(list(response.context['entries']), [])
+        response = self.client.get(self.url)
+        verify_empty(response)
+        self.make_entries()
+        data = {
+            'year': 2011,
+            'month': 4,
+        }
+        response = self.client.get(self.url, data)
+        verify_empty(response)
+
+    def testCurrentProjectTimesheet(self):
+        self.client.login(username='superuser', password='abc')
+        self.make_entries()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['project'], self.p1)
+        self.assertEqual(len(response.context['entries']), 1)
+        self.assertEqual(response.context['total'], Decimal(1))
+        user_entry = response.context['user_entries'][0]
+        self.assertEqual(user_entry['user__last_name'], self.user.last_name)
+        self.assertEqual(user_entry['user__first_name'], self.user.first_name)
+        self.assertEqual(user_entry['sum'], Decimal(1))
+
+    def testOldProjectTimesheet(self):        
+        self.client.login(username='superuser', password='abc')
+        self.make_entries()
+        data = {
+            'year': 2011,
+            'month': 1,
+        }
+        response = self.client.get(self.url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['project'], self.p1)
+        self.assertEqual(len(response.context['entries']), 3)
+        self.assertEqual(response.context['total'], Decimal(3))
+        user_entry0 = response.context['user_entries'][0]
+        user_entry1 = response.context['user_entries'][1]        
+        self.assertEqual(user_entry0['user__last_name'], self.user.last_name)
+        self.assertEqual(user_entry0['user__first_name'], self.user.first_name)
+        self.assertEqual(user_entry0['sum'], Decimal(2))
+        self.assertEqual(user_entry1['user__last_name'], self.user2.last_name)
+        self.assertEqual(user_entry1['user__first_name'],
+                         self.user2.first_name
+        )
+        self.assertEqual(user_entry1['sum'], Decimal(1))
