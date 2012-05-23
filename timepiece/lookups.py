7a6127bcaf5a442c74dde722418e7e14d57d8ab3
Omar Estrella <omar@caktusgroup.com>
2012-05-23 13:01:50 -0400

    refs #175, #176 - Lookups comply with selectable 0.4.1; updated QuickLookup

--- timepiece/lookups.py
+++ timepiece/lookups.py
@@ -11,12 +11,11 @@ from timepiece import models as timepiece
 
 class ProjectLookup(ModelLookup):
     model = timepiece.Project
-    search_field = 'name__icontains'
+    search_fields = ('name__icontains',)
 registry.register(ProjectLookup)
 
-
-class UserLookup(object):
-    def get_query(self, q, request):
+class UserLookup(LookupBase):
+    def get_query(self, request, q):
         """
         return a query set.  you also have access to request.user if needed
         """
@@ -60,44 +59,53 @@ class SearchResult(object):
         self.name = name
 
 
-class QuickLookup(object):
-    def get_query(self, q, request):
+class QuickLookup(LookupBase):
+    def get_query(self, request, q):
         """
         return a query set (or a fake one).  you also have access to
         request.user if needed
         """
         results = []
+        
         users = auth_models.User.objects.filter(
             Q(first_name__icontains=q) |
             Q(last_name__icontains=q) |
             Q(email__icontains=q)
         ).select_related().order_by('last_name')[:10]
+
         for user in users:
-            name = '%s %s' % (user.first_name, user.last_name)
+            name = user.get_full_name()
             results.append(
                 SearchResult(user.pk, 'individual', name)
             )
-        for project in timepiece.Project.objects.filter(
-                name__icontains=q,
-            ).select_related():
+
+        projects = timepiece.Project.objects.filter(
+            name__icontains=q,
+        ).select_related()[:10]
+
+        for project in projects:
             results.append(
                 SearchResult(project.pk, 'project', project.name)
             )
-        for business in timepiece.Business.objects.filter(
-                name__icontains=q,
-            ).select_related():
+
+        businesses = timepiece.Business.objects.filter(
+            name__icontains=q,
+        ).select_related()[:10]
+
+        for business in businesses:
             results.append(
                 SearchResult(business.pk, 'business', business.name)
             )
+        
         results.sort(lambda a, b: cmp(a.name, b.name))
         return results
 
-    def format_item(self, item):
+    '''def format_item(self, item):
         """
         simple display of an object when it is displayed in the list of
         selected objects
         """
-        return item.name
+        return item.name'''
 
     def format_result(self, item):
         """
@@ -105,6 +113,15 @@ class QuickLookup(object):
         may contain html and multi-lines
         """
         return u"<span class='%s'>%s</span>" % (item.type, item.name)
+
+    def get_item_label(self, item):
+        return self.format_result(item)
+
+    def get_item_id(self, item):
+        return item.pk
+
+    def get_item_value(self, item):
+        return item.name
 
     def get_objects(self, ids):
         """
@@ -122,3 +139,4 @@ class QuickLookup(object):
             elif type == 'individual':
                 results.append(auth_models.User.objects.get(pk=pk))
         return results
+registry.register(QuickLookup)
