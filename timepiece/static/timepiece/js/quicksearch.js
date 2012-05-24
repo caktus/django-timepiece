3e433bef9aad01f84002c30842a52f486a368853
Omar Estrella <omar@caktusgroup.com>
2012-05-24 10:41:38 -0400

    refs #176 - Removed quicksearch file and from template

--- timepiece/static/timepiece/js/quicksearch.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    $(document).ready(function() {
-        $('input[name="quick_search_0"]').bind('autocompleteselect', function(e, ui) {
-            $(this).parents('form').submit();
-        }).bind('autocompletefocus', function(e, ui) {
-            $(this).val(ui.item.value);
-        });
-    });
-});
