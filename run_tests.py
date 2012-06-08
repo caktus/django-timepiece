4c608b1fc62be89325c24a7de3713989cb36d190
Omar Estrella <omar@caktusgroup.com>
2012-06-08 14:52:19 -0400

    refs #234 - Changed python/django support; updated run_tests to run django tests by default

--- run_tests.py
+++ run_tests.py
@@ -1,43 +1,58 @@
+import optparse
+
 from django.conf import settings
 from django.core.management import call_command
 
-databases = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'caktus',
-        'USER': 'omarestrella',
-        'PASSWORD': 'nothing',
-        'HOST': '',
-        'PORT': '',
-    }
-}
-project_apps = ('timepiece',)
-jenkins_tasks = (
-    'django_jenkins.tasks.with_coverage',
-    'django_jenkins.tasks.django_tests',
-    'django_jenkins.tasks.run_pep8',
-)
-
-settings.DATABASES = databases
-settings.INSTALLED_APPS += ('django_jenkins',)
-settings.PROJECT_APPS = project_apps
-settings.JENKINS_TASKS = jenkins_tasks
+parser = optparse.OptionParser()
+opts, args = parser.parse_args()
 
 
-def runtests():
-    kwargs = {
-        'pep8-exclude': 'migrations',
-        'pep8-select': '',
-        'pep8-ignore': '',
-        'pep8-max-line-length': 80,
-        'coverage-exclude': 'timepiece.migrations',
-        'coverage_with_migrations': False,
-        'coverage_html_report_dir': '',
-        'coverage_excludes': [],
-        'coverage_measure_branch': False,
-        'coverage_rcfile': '',
-    }
-    call_command('jenkins', **kwargs)
+class TestRunner:
+    def __init__(self, *args, **kwargs):
+        self.databases = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'caktus',
+                'USER': 'omarestrella',
+                'PASSWORD': 'nothing',
+                'HOST': '',
+                'PORT': '',
+            }
+        }
+        self.project_apps = ('timepiece',)
+        self.jenkins_tasks = (
+            'django_jenkins.tasks.with_coverage',
+            'django_jenkins.tasks.django_tests',
+            'django_jenkins.tasks.run_pep8',
+        )
+
+    def run_jenkins_tests(self):
+        settings.DATABASES = self.databases
+        settings.INSTALLED_APPS += ('django_jenkins',)
+        settings.PROJECT_APPS = self.project_apps
+        settings.JENKINS_TASKS = self.jenkins_tasks
+
+        kwargs = {
+            'pep8-exclude': 'migrations',
+            'pep8-select': '',
+            'pep8-ignore': '',
+            'pep8-max-line-length': 80,
+            'coverage-exclude': 'timepiece.migrations',
+            'coverage_with_migrations': False,
+            'coverage_html_report_dir': '',
+            'coverage_excludes': [],
+            'coverage_measure_branch': False,
+            'coverage_rcfile': '',
+        }
+        call_command('jenkins', **kwargs)
+
+    def run_django_tests(self):
+        settings.DATABASES = self.databases
+        call_command('test', 'timepiece')
 
 if __name__ == '__main__':
-    runtests()
+    runner = TestRunner()
+    if 'jenkins' in args:
+        runner.run_jenkins_tests()
+    else:
+        runner.run_django_tests()
