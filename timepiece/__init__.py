029671d7151c00e5ccfce25f33f77f5c72033fe1
Colin Copeland <copelco@caktusgroup.com>
2012-06-15 14:38:38 -0400

    0.4.2

--- timepiece/__init__.py
+++ timepiece/__init__.py
@@ -1,1 +1,24 @@
-__version__ = "0.4.1"
+"""
+django-timepiece is a multi-user application for tracking people's time on projects.
+"""
+
+__version_info__ = {
+    'major': 0,
+    'minor': 4,
+    'micro': 2,
+    'releaselevel': 'final',
+}
+
+def get_version():
+    """
+    Return the formatted version information
+    """
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final':
+        vers.append('%(releaselevel)s' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
