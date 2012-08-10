25e305748e62b43049df14a8f2e3f4d03e0d3f48
Omar Estrella <omar@caktusgroup.com>
2012-08-10 11:22:03 -0400

    Update widths to prevent all clipping; correctly position text

--- timepiece/static/timepiece/js/dashboard/weekly_detail.js
+++ timepiece/static/timepiece/js/dashboard/weekly_detail.js
@@ -4,13 +4,18 @@ function ProgressBar(loc, width, height, label) {
     this.height = height;
     this.label = label;
 
+    // Settings
+    this.draw_width = this.width - 10; // Draw inside the svg without clipping
+    this.edge_offset = 5; // Come off the edge
+
     // Append our svg for drawing
     this.chart = d3.select(loc).append('svg')
+        .attr('width', this.draw_width)
         .attr('class', 'hoursChart');
 }
 
-ProgressBar.prototype.draw = function(worked, remaining) {
-    var width = $(this.loc).width(),
+ProgressBar.prototype.draw = function(hours_worked, hours_remaining) {
+    var width = this.draw_width,
         offset = 0;
 
     // Calculate offset before drawing border
@@ -29,30 +34,34 @@ ProgressBar.prototype.draw = function(worked, remaining) {
             .text(this.label);
     }
 
+    var actual_offset = offset + this.edge_offset;
+
     this.border = this.chart
         .append('rect')
-        .attr('x', offset).attr('y', 0)
+        .attr('x', actual_offset).attr('y', 0)
         .attr('height', this.height)
-        .attr('width', this.width - offset);
+        .attr('width', width - 10); // Prevent border clipping
 
     var bar = this,
         chart = this.chart,
-        total = worked + remaining;
+        total = hours_worked + hours_remaining;
 
+    // Draw the hours worked bar first
     bar.worked = chart.append('rect')
         .style('stroke-width', '0px')
         .style('stroke', 'none')
         .attr('class', 'workedHours')
         .attr('height', this.height - 1)
-        .attr('y', 1).attr('x', offset);
+        .attr('y', 1).attr('x', actual_offset + 1);  // Account for stroke
 
     bar.worked.transition()
         .delay(100)
         .duration(750)
-        .attr('width', (worked / total) * width)
+        .attr('width', (hours_worked / total) * width - this.edge_offset)
         .style('fill', 'steelblue')
         .each('end', drawRemaining);
 
+    // Draw the remaining bar after drawing the worked bar
     function drawRemaining() {
         bar.remaining = chart.append('rect')
             .style('stroke-width', '0px')
@@ -66,32 +75,48 @@ ProgressBar.prototype.draw = function(worked, remaining) {
         bar.remaining.transition()
             .delay(100)
             .duration(750)
-            .attr('width', (remaining / total) * width)
+            .attr('width', (hours_remaining / total) * width - bar.edge_offset - 1)
             .each('end', drawText);
     }
 
+    // Calculate the correct positioning
+    function textPosition(bar, text) {
+        var box = text.node().getBBox();
+
+        var xpos = Number(bar.attr('x')) +
+            Number(bar.attr('width')) / 2 - box.width / 2;
+        
+        var ypos = Number(bar.attr('height')) / 2 + box.height / 4;
+
+        return {
+            'x': xpos,
+            'y': ypos
+        };
+    }
+
+    // Display text after the bars have been drawn
     function drawText() {
-        var text_width = 30;
-
-        chart.append('text')
+        var worked = chart.append('text')
             .attr('font-size', '16px')
             .attr('fill', '#FFFFFF')
             .attr('font-weight', 'bolder')
-            .attr('x', function() {
-                return Number(bar.worked.attr('x')) + Number(bar.worked.attr('width')) / 2 - text_width;
-            })
-            .attr('y', Number(bar.worked.attr('height')) / 2 + 7)
-            .text(worked + ' worked');
+            .text(hours_worked + ' worked');
 
-        chart.append('text')
+        var worked_pos = textPosition(bar.worked, worked);
+
+        worked.attr('x', worked_pos.x)
+            .attr('y', worked_pos.y);
+
+        var remaining = chart.append('text')
             .attr('font-size', '16px')
             .attr('fill', '#FFFFFF')
             .attr('font-weight', 'bolder')
-            .attr('x', function() {
-                return Number(bar.remaining.attr('x')) + Number(bar.remaining.attr('width')) / 2 - text_width;
-            })
-            .attr('y', Number(bar.remaining.attr('height')) / 2 + 7)
-            .text(remaining + ' remaining');
+            .text(hours_remaining + ' remaining');
+
+        var rem_pos = textPosition(bar.remaining, remaining);
+
+        remaining.attr('x', rem_pos.x)
+            .attr('y', rem_pos.y);
     }
 };
 
