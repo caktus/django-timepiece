0c7f7df297ece9749bf5be0a2e11a473410275a9
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-09-26 16:34:49 -0400

    refs #458 - Updated example_project Removed extraneous template, added registration templates, added missing JS, and use postgres.

--- example_project/settings.py
+++ example_project/settings.py
@@ -2,6 +2,7 @@
 
 from os import path
 
+
 PROJECT_PATH = path.abspath('%s' % path.dirname(__file__))
 
 DEBUG = True
@@ -15,12 +16,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timepiece.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timepiece',
+        'USER': 'timepiece',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -143,8 +144,6 @@ COMPRESS_PRECOMPILERS = (
 )
 
 COMPRESS_ROOT = '%s/static/' % PROJECT_PATH
-
-INTERNAL_IPS = ('127.0.0.1',)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
