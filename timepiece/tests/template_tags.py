18bf77402fe1fedc17a4d6719fb9146a8bdd35f8
Caleb Smith <csmith@caktusgroup.com>
2012-11-05 13:03:18 -0500

    Refs #505 - Added tests for humanize_hours/seconds template tags

---
+++ timepiece/tests/template_tags.py
@@ -0,0 +1,50 @@
+from timepiece.templatetags import timepiece_tags as tags
+from timepiece.tests.base import TimepieceDataTestCase
+
+
+class HumanizeSecondsTestCase(TimepieceDataTestCase):
+
+    def test_zero(self):
+        self.assertEqual(tags.humanize_seconds(0), '0',
+            "Given 0 seconds, but doesn't return '0'"
+        )
+
+    def test_default_format(self):
+        seconds_display = tags.humanize_seconds((5.5 * 3600) + 3)
+        self.assertEqual(seconds_display, '05:30:03',
+            "Given (5.5 * 3600) + 3 seconds, should return 5h30m3s in %H:%M:%S"
+        )
+
+    def test_format_specified(self):
+        seconds_display = tags.humanize_seconds(5.75 * 3600, '%H:%M')
+        self.assertEqual(seconds_display, '05:45',
+            "Given '%H:%M' as second argument, should return in that format"
+        )
+        seconds_display = tags.humanize_seconds(5.75 * 3600, '%H:%M:%S')
+        self.assertEqual(seconds_display, '05:45:00',
+            "Given '%H:%M:%S' as second argument, should return in that format"
+        )
+
+
+class HumanizeHoursTestCase(TimepieceDataTestCase):
+
+    def test_zero(self):
+        self.assertEqual(tags.humanize_hours('0'), '0',
+            "Given 0 hours, should return '0'"
+        )
+
+    def test_default_format(self):
+        hours_display = tags.humanize_hours('3.25')
+        self.assertEqual(hours_display, '03:15',
+            "Given 3.25 hours, should return 3h15m in %H:%M"
+        )
+
+    def test_format_specified(self):
+        hours_display = tags.humanize_hours('1.75', '%H:%M')
+        self.assertEqual(hours_display, '01:45',
+            "Given '%H:%M' as second argument, should return in that format"
+        )
+        hours_display = tags.humanize_hours('1.76', '%H:%M:%S')
+        self.assertEqual(hours_display, '01:45:36',
+            "Given '%H:%M:%S' as second argument, should return in that format"
+        )
