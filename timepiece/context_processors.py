38c2fef1578778a8af332f85c1a7b3a0487ec270
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-11-16 09:11:48 -0500

    refs #534 - Limit # of projects in quick clock in dropdown Some users have many projects, and the list overflows off of the screen. This commit limits the list to the 10 projects most recently clocked in to, plus paid leave projects.

--- timepiece/context_processors.py
+++ timepiece/context_processors.py
@@ -1,6 +1,8 @@
-from timepiece import models as timepiece
+from django.db.models import Q
+
 from timepiece import utils
 from timepiece.forms import QuickSearchForm
+from timepiece.models import Project, Entry
 
 
 def quick_search(request):
@@ -11,22 +13,40 @@ def quick_search(request):
 
 def quick_clock_in(request):
     user = request.user
-    projects = []
+    work_projects = []
+    leave_projects = []
+
     if user.is_authenticated() and user.is_active:
-        vacation_ids = utils.get_setting(
+        q = Q(status__enable_timetracking=True)
+        q &= Q(type__enable_timetracking=True)
+
+        # Display all active paid leave projects that the user is assigned to.
+        leave_ids = utils.get_setting(
                 'TIMEPIECE_PAID_LEAVE_PROJECTS').values()
-        vacation_projects = timepiece.Project.objects.filter(
-                users=user, id__in=vacation_ids) \
-            .order_by('name').values('name', 'id')
-        work_projects = timepiece.Project.objects.filter(
-                users=user, status__enable_timetracking=True,
-                type__enable_timetracking=True) \
-            .exclude(id__in=vacation_ids) \
-            .order_by('name').values('name', 'id')
-        projects = list(work_projects) + list(vacation_projects)
+        lq = Q(users=user) & Q(id__in=leave_ids) & q
+        leave_projects = Project.objects.filter(lq).order_by('name')
+
+        # Get all projects this user has clocked in to.
+        entries = Entry.objects.filter(user=user)
+        project_ids = list(entries.values_list('project', flat=True))
+
+        # Narrow to projects which can still be clocked in to.
+        pq = Q(id__in=project_ids) & q
+        valid_projects = Project.objects.filter(pq).exclude(id__in=leave_ids)
+        valid_ids = list(valid_projects.values_list('id', flat=True))
+
+        # Display the 10 projects this user most recently clocked into.
+        work_ids = []
+        for i in project_ids:
+            if len(work_ids) > 10:
+                break;
+            if i in valid_ids and i not in work_ids:
+                work_ids.append(i)
+        work_projects = [valid_projects.get(pk=i) for i in work_ids]
 
     return {
-        'quick_clock_in_projects': projects,
+        'leave_projects': leave_projects,
+        'work_projects': work_projects,
     }
 
 
