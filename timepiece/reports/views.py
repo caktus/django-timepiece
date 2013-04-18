8216aae2b3ebe15790848ac13de1a9dec369c88b
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-04-18 13:32:22 -0400

    Migrate reports templates

--- timepiece/reports/views.py
+++ timepiece/reports/views.py
@@ -63,7 +63,7 @@ def report_payroll_summary(request):
                         .values_list(*user_values).distinct()
     unapproved = entries.filter(status='verified') \
                         .values_list(*user_values).distinct()
-    return render(request, 'timepiece/reports/payroll_summary.html', {
+    return render(request, 'reports/payroll_summary.html', {
         'from_date': from_date,
         'year_month_form': year_month_form,
         'date_headers': date_headers,
@@ -201,7 +201,7 @@ class ReportMixin(object):
 
 
 class HourlyReport(ReportMixin, CSVMixin, TemplateView):
-    template_name = 'timepiece/reports/hourly.html'
+    template_name = 'reports/hourly.html'
 
     def convert_context_to_csv(self, context):
         """Convert the context dictionary into a CSV file."""
@@ -313,7 +313,7 @@ class HourlyReport(ReportMixin, CSVMixin, TemplateView):
 
 
 class BillableHours(ReportMixin, TemplateView):
-    template_name = 'timepiece/reports/billable_hours.html'
+    template_name = 'reports/billable_hours.html'
 
     @property
     def defaults(self):
@@ -459,7 +459,7 @@ def report_productivity(request):
                 writer.writerow(row)
             return response
 
-    return render(request, 'timepiece/reports/productivity.html', {
+    return render(request, 'reports/productivity.html', {
         'form': form,
         'report': json.dumps(report, cls=DecimalEncoder),
         'type': organize_by or '',
