314e42a22fdd1eb576515977b25a8ea9afae1e1b
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-05-08 17:25:10 -0400

    Fixes #680 - Allow ordering by project in admin

--- timepiece/entries/admin.py
+++ timepiece/entries/admin.py
@@ -19,25 +19,22 @@ class ActivityGroupAdmin(admin.ModelAdmin):
 
 class EntryAdmin(admin.ModelAdmin):
     model = Entry
-    list_display = ('user',
-                    'project',
-                    'location',
-                    'project_type',
-                    'activity',
-                    'start_time',
-                    'end_time',
-                    'hours',
-                    'is_closed',
-                    'is_paused',
-                    )
+    list_display = ('user', '_project', 'location', 'project_type', 'activity',
+            'start_time', 'end_time', 'hours', 'is_closed', 'is_paused')
     list_filter = ['activity', 'project__type', 'user', 'project']
     search_fields = ['user__first_name', 'user__last_name', 'project__name',
-                     'activity__name', 'comments']
+            'activity__name', 'comments']
     date_hierarchy = 'start_time'
     ordering = ('-start_time',)
 
     def project_type(self, entry):
         return entry.project.type
+
+    def _project(self, obj):
+        """Use a proxy to avoid an infinite loop from ordering."""
+        return unicode(obj.project)
+    _project.admin_order_field = 'project__name'
+    _project.short_description = 'Project'
 
 
 class LocationAdmin(admin.ModelAdmin):
@@ -45,12 +42,18 @@ class LocationAdmin(admin.ModelAdmin):
 
 
 class ProjectHoursAdmin(admin.ModelAdmin):
-    list_display = ('_user', 'project', 'week_start', 'hours', 'published')
+    list_display = ('_user', '_project', 'week_start', 'hours', 'published')
 
     def _user(self, obj):
         return obj.user.get_name_or_username()
     _user.short_description = 'User'
     _user.admin_order_field = 'user__last_name'
+
+    def _project(self, obj):
+        """Use a proxy to avoid an infinite loop from ordering."""
+        return unicode(obj.project)
+    _project.admin_order_field = 'project__name'
+    _project.short_description = 'Project'
 
 
 admin.site.register(Activity, ActivityAdmin)
