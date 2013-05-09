2ace2cfa47b57ca0b1ff146f3bd09f6e0a874d53
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-05-09 16:10:15 -0400

    Add ContentType and Permission to admin

---
+++ timepiece/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    list_filter = ['content_type__app_label']
+
+
+class ContentTypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'app_label', 'model']
+    list_filter = ['app_label']
+
+
+admin.site.register(Permission, PermissionAdmin)
+admin.site.register(ContentType, ContentTypeAdmin)
