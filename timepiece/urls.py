1a82e8d49924c53d240ef7d7fadb7945bf2f84cc
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-06-25 16:09:36 -0400

    Fix syntax error

--- timepiece/urls.py
+++ timepiece/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Redirect the base URL to the dashboard.
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('dashboard')),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('dashboard'))),
 
     url('', include('timepiece.crm.urls')),
     url('', include('timepiece.contracts.urls')),
