e1d2838301a562f49fc0c1dafca70c9b46210829
Rebecca Lovewell <rebecca@caktusgroup.com>
2013-09-04 09:47:57 -0400

    Updated quick search tests

--- timepiece/crm/tests/__init__.py
+++ timepiece/crm/tests/__init__.py
@@ -1,5 +1,5 @@
 from .test_businesses import *
 from .test_projects import *
+from .test_quick_search import *
 from .test_relationships import *
-from .test_search import *
 from .test_users import *
