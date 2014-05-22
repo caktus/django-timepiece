402fd2fd0e89d4c7e90357943aab558e8944774e
Wray Bowling <wbowling@caktusgroup.com>
2014-05-22 13:31:02 -0400

    removed comments in JS restored {%block javascript%} in header & moved sticky header JS to {%block extrajs %} closed scroll-x-axis div on schedule

--- timepiece/static/timepiece/js/sticky_table_header.js
+++ timepiece/static/timepiece/js/sticky_table_header.js
@@ -1,5 +1,5 @@
 var tables = $('table');
-var i, difference_top, difference_left, navbarHeight;
+var i, navbarHeight;
 
 function calculateNavbar(){
 	if( window.innerWidth >= 840){
@@ -23,8 +23,7 @@ calculateHeaderCutoffs();
 function positionHeaders(){
 	for (i = 0; i < tables.length; i++) {
 		var scroll = document.documentElement.scrollTop || document.body.scrollTop;
-		difference_top = scroll + navbarHeight - tables[i].dataset.top;
-		//console.log('i', i, 'scroll', scroll, 'navbar', navbarHeight, 'table top', tables[i].dataset.top, 'diff', difference_top );
+		var difference_top = scroll + navbarHeight - tables[i].dataset.top;
 
 		if(difference_top <= 0){
 			tables[i].querySelector('thead').style.webkitTransform = 'translate(0px,0px)';
@@ -39,22 +38,19 @@ function positionHeaders(){
 			tables[i].querySelector('thead').style.MozTransform = 'translate(0px,'+ tables[i].dataset.bottom +'px)';
 		}
 	}
-//	console.log( difference_top );
 }
 positionHeaders();
 
 function calculateColumnCutoffs(){
 	for (i = 0; i < tables.length; i++) {
 		tables[i].dataset.left = $(tables[i]).find('tr td:first').offset().left;
-		//console.log(tables[i].dataset.left);
-		//tables[i].dataset.right = $(tables[i]).height() - $(tables[i]).children('thead').height() - $(tables[i]).children('tbody tr:last').height();
 	}
 }
 calculateColumnCutoffs();
 
 function positionColumns(){
 	for (i = 0; i < tables.length; i++) {
-		difference_left = $(tables[i]).parent().scrollLeft();// - tables[i].dataset.left;
+		var difference_left = $(tables[i]).parent().scrollLeft();// - tables[i].dataset.left;
 
 		if(difference_left <= 0){
 			$(tables[i]).find('tbody tr > :first-child').css('WebkitTransform', 'translate(0px,0px)');
@@ -63,18 +59,13 @@ function positionColumns(){
 		if(difference_left > 0){
 			$(tables[i]).find('tbody tr > :first-child').css('WebkitTransform', 'translate('+ difference_left +'px, 0px)');
 			$(tables[i]).find('tbody tr > :first-child').css('MozTransform', 'translate('+ difference_left +'px, 0px)');
-		}/*
-		if(difference_left > tables[i].dataset.bottom){
-			tables[i].querySelector('th:first-child,td:first-child').style.webkitTransform = 'translate(0px,'+ tables[i].dataset.bottom +'px)';
-			tables[i].querySelector('th:first-child,td:first-child').style.MozTransform = 'translate(0px,'+ tables[i].dataset.bottom +'px)';
-		}*/
+		}
 	}
-//	console.log( difference_left );
 }
 positionColumns();
 
 for (i = 0; i < tables.length; i++) {
-	$(tables[i]).parent().on('scroll', function(){ positionColumns(); });
+	$(tables[i]).parent('.scroll-x-axis').on('scroll', function(){ positionColumns(); });
 }
 
 window.onscroll = function(){ positionHeaders(); };
@@ -84,8 +75,3 @@ window.onresize = function(){
 	calculateHeaderCutoffs();
 	positionHeaders();
 };
-/*
-window.onresize = function(){
-	console.log('scroll top', document.body.scrollTop);
-}
-*/
