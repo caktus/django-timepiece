f2478e1fb388277191d0d8f6a43ff68691073d06
Conrado Buhrer <conrado@buhrer.net>
2014-10-28 19:56:00 -0200

    fixed: added missing json import

--- timepiece/utils/http.py
+++ timepiece/utils/http.py
@@ -3,6 +3,7 @@ from django import VERSION as DJANGO_VERSION
 if DJANGO_VERSION[0] > 1 or (DJANGO_VERSION[0]==1 and DJANGO_VERSION[1] >= 7):
     from django.http import JsonResponse
 else:
+    import json
     from django.http import HttpResponse
 
     def JsonResponse(data, encoder=None, **kwargs):
