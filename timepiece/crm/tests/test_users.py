57747d9fe12919b01c934245506ca7a32a4f3808
Rebecca <rebecca@caktusgroup.com>
2015-01-15 21:44:13 -0500

    Update Python/Django: Remove use of assertItemsEqual

--- timepiece/crm/tests/test_users.py
+++ timepiece/crm/tests/test_users.py
@@ -97,8 +97,9 @@ class TestCreateUser(ViewTestMixin, TestCase):
         self.assertEquals(obj.email, self.post_data['email'])
         self.assertEquals(obj.is_active, self.post_data['is_active']),
         self.assertEquals(obj.is_staff, self.post_data['is_staff']),
-        self.assertItemsEqual(obj.groups.values_list('pk', flat=True),
-                self.post_data['groups'])
+        groups = obj.groups.values_list('pk', flat=True)
+        self.asserEqual(len(groups), len(self.post_data['groups']))
+        self.assertEqual(sorted(groups), sorted(self.post_data['groups']))
         self.assertTrue(obj.check_password(self.post_data['password1']))
 
     def test_nonmatching_passwords(self):
@@ -209,8 +210,10 @@ class TestEditUser(ViewTestMixin, TestCase):
         self.assertEquals(obj.email, self.obj.email)
         self.assertEquals(obj.is_active, self.obj.is_active)
         self.assertEquals(obj.is_staff, self.obj.is_staff)
-        self.assertItemsEqual(obj.groups.values_list('pk', flat=True),
-                self.obj.groups.values_list('pk', flat=True))
+        groups1 = obj.groups.values_list('pk', flat=True)
+        groups2 = self.obj.groups.values_list('pk', flat=True)
+        self.assertEqual(len(groups1), len(groups2))
+        self.assertEqual(sorted(groups1), sorted(groups2))
 
     def test_get_no_permission(self):
         """Permission is required for this view."""
@@ -256,8 +259,9 @@ class TestEditUser(ViewTestMixin, TestCase):
         self.assertEquals(obj.email, self.post_data['email'])
         self.assertEquals(obj.is_active, self.post_data['is_active'])
         self.assertEquals(obj.is_staff, self.post_data['is_staff'])
-        self.assertItemsEqual(obj.groups.values_list('pk', flat=True),
-                self.post_data['groups'])
+        groups = obj.groups.values_list('pk', flat=True)
+        self.assertEqual(len(groups), len(self.post_data['groups']))
+        self.assertEqual(sorted(groups), sorted(self.post_data['groups']))
 
     def test_post_invalid(self):
         """Invalid POST should not edit the object."""
