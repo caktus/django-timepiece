0b71219f146a166e9b9b787692b46bbc199e1099
Matthew Schoeneck <matt.schoeneck@aacengineering.com>
2015-08-13 23:00:13 +0000

    added the functionality to directly edit the user with any contact changes if there is an underlying user.

--- timepiece/crm/forms.py
+++ timepiece/crm/forms.py
@@ -387,8 +387,25 @@ class CreateEditContactForm(forms.ModelForm):
         super(CreateEditContactForm, self).__init__(*args, **kwargs)
         self.fields['lead_source'].choices = self.EMPLOYEE_CHOICES
 
+
         # Check if Contact is User and edit underlying info if so
 
+    def save(self, commit=True):
+        instance_contact = super(CreateEditContactForm, self).save(commit=False)
+        instance_user=instance_contact.user
+
+        if instance_user is not None:
+            instance_user.first_name = instance_contact.first_name
+            instance_user.last_name = instance_contact.last_name
+            instance_user.email = instance_contact.email
+
+            if commit:
+                instance_user.save()
+
+        if commit:
+            instance_contact.save()
+            #self.save_m2m()
+        return instance_contact
 
 
 class AddContactNoteForm(forms.ModelForm):
